c58acd4e2d5c7d16478ee3b1e8b6d0110de991c7
Josh Cook <joshuacook0023@gmail.com>
2019-02-20 22:42:15 -0500

    doc: roxygen for factor_is_greater and which_level

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,22 @@
 
 # return is factor a is greater than factor b (a and b are same-length vectors)
+
+
+#' Determines if the level of a is greater than that of b
+#'
+#' @param a,b Two same-length, same-leveled vectors of type \code{factor}
+#' @return a single boolean vector of type \code{logical}
+#' @examples
+#' first <- c("J", "O", "S", "H")
+#' last <- c("C", "O", "O", "K")
+#' first <- factor(first, LETTERS)
+#' last <- factor(last, LETTERS)
+#'
+#' factor_is_greater(first, last)
+#'
+#' #> [1]  TRUE FALSE  TRUE FALSE
 factor_is_greater <- function(a, b) {
-    # TODO: take an expression instead of only computing greater than
+    stopifnot(class(a) == "factor" & class(b) == "factor")
     ai <- which_level(a)
     bi <- which_level(b)
     return(ai > bi)
@@ -9,31 +24,15 @@ factor_is_greater <- function(a, b) {
 
 
 
-# level (as int) of a factor (x is a vector)
+#' Determine the level of a value in a vector of type \code{factor}
+#'
+#' @param x vectors of type \code{factor}
+#' @return a vector holding the corresponding level of the input factors
+#' @examples
+#' first <- factor(c("J", "O", "S", "H"), LETTERS)
+#' which_level(first)
+#' #> [1] 10 15 19  8
 which_level <- function(x) {
     map_int(x, ~ str_which(levels(x), as.character(.x)))
 }
 
-
-
-# duplicates .data but swtiching the values in .x and .y
-col_swap <- function(.data, .x, .y) {
-    .x <- enquo(.x)
-    .y <- enquo(.y)
-    y_new <- eval_tidy(.x, .data)
-    x_new <- eval_tidy(.y, .data)
-    mutate(.data,
-           !!.x := x_new,
-           !!.y := y_new)
-}
-
-
-# returns a tibbles with g1 <=> g2
-add_diag_rows <- function(.data, ..., .grp = NA) {
-    all_vals <- unique(c(tib$g1, tib$g2))
-    diag_tib <- tib %>%
-        slice(1:length(all_vals)) %>%
-        mutate(g1 = all_vals,
-               g2 = all_vals)
-    return(diag_tib)
-}
