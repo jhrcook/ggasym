c6d355f7ddec7b6238f7db178222035f7170db6b
Josh Cook <joshuacook0023@gmail.com>
2019-02-26 08:29:26 -0500

    feat: working model; need to fully develop still

--- R/geom_asymmat.R
+++ R/geom_asymmat.R
@@ -74,13 +74,54 @@ GeomAsymmat <- ggproto("GeomAsymmat", GeomRect,
                     extra_params = c("na.rm", "which_triangle"),
 
                     setup_data = function(data, params) {
+                        # browser()
                         data$width <- data$width %||% params$width %||% resolution(data$x, FALSE)
                         data$height <- data$height %||% params$height %||% resolution(data$y, FALSE)
+                        # if (params$which_triangle == "tl") data$fill <- data$fill_tl
+                        # else if (params$which_triangle == "br") data$fill <- data$fill_br
 
                         transform(data,
                                   xmin = x - width / 2,  xmax = x + width / 2,  width = NULL,
                                   ymin = y - height / 2, ymax = y + height / 2, height = NULL
                         )
+                    },
+
+                    draw_panel = function(self, data, panel_params, coord) {
+                        if (!all(is.na(data$fill_tl))) {
+                            data$fill <- data$fill_tl
+                        } else if (!all(is.na(data$fill_br))) {
+                            data$fill <- data$fill_br
+                        }
+                        if (!coord$is_linear()) {
+                            aesthetics <- setdiff(
+                                names(data), c("x", "y", "xmin", "xmax", "ymin", "ymax")
+                            )
+
+                            polys <- lapply(split(data, seq_len(nrow(data))), function(row) {
+                                poly <- rect_to_poly(row$xmin, row$xmax, row$ymin, row$ymax)
+                                aes <- new_data_frame(row[aesthetics])[rep(1,5), ]
+
+                                GeomPolygon$draw_panel(cbind(poly, aes), panel_params, coord)
+                            })
+
+                            ggname("bar", do.call("grobTree", polys))
+                        } else {
+                            coords <- coord$transform(data, panel_params)
+                            ggname("geom_asymmat", grid::rectGrob(
+                                coords$xmin, coords$ymax,
+                                width = coords$xmax - coords$xmin,
+                                height = coords$ymax - coords$ymin,
+                                default.units = "native",
+                                just = c("left", "top"),
+                                gp = grid::gpar(
+                                    col = coords$colour,
+                                    fill = alpha(coords$fill, coords$alpha),
+                                    lwd = coords$size * .pt,
+                                    lty = coords$linetype,
+                                    lineend = "butt"
+                                )
+                            ))
+                        }
                     },
 
                     default_aes = aes(fill_tl = NA, fill_br = NA,
@@ -92,7 +133,10 @@ GeomAsymmat <- ggproto("GeomAsymmat", GeomRect,
                     draw_key = draw_key_polygon
 )
 
-
+ggname <- function(prefix, grob) {
+    grob$name <- grid::grobName(grob, prefix)
+    grob
+}
 
 
 
@@ -101,11 +145,14 @@ scale_fill_tl_gradient <- function(..., low = "#132B43", high = "#56B1F7",
                                    na.value = "grey50",
                                    guide = "colourbar",
                                    aesthetics = "fill_tl") {
-    continuous_scale(aesthetics,
+    cs <- continuous_scale(aesthetics,
                      "gradient",
                      seq_gradient_pal(low, high, space),
                      na.value = na.value,
                      guide = guide, ...)
+    cs$guide <- match.fun(paste0("guide_", cs$guide))()
+    cs$guide$available_aes[stringr::str_which(cs$guide$available_aes, "fill")] <- "fill_tl"
+    return(cs)
 }
 
 scale_fill_br_gradient <- function(..., low = "#132B43", high = "#56B1F7",
@@ -113,12 +160,17 @@ scale_fill_br_gradient <- function(..., low = "#132B43", high = "#56B1F7",
                                    na.value = "grey50",
                                    guide = "colourbar",
                                    aesthetics = "fill_br") {
-    continuous_scale(aesthetics,
+    cs <- continuous_scale(aesthetics,
                      "gradient",
                      seq_gradient_pal(low, high, space),
                      na.value = na.value,
                      guide = guide, ...)
+    cs$guide <- match.fun(paste0("guide_", cs$guide))()
+    cs$guide$available_aes[stringr::str_which(cs$guide$available_aes, "fill")] <- "fill_br"
+    return(cs)
 }
+
+
 
 
 # FOR REFERENCE
