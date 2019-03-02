6f309a01f0296e05c4bc51b40a254cfc13fce0cf
Josh Cook <joshuacook0023@gmail.com>
2019-03-01 22:16:09 -0500

    doc: creation and doc for mouse liver expression dataset

--- R/data.R
+++ R/data.R
@@ -18,3 +18,40 @@
 #' @examples
 #' enrichment_data
 "enrichment_data"
+
+#' Diet of mice for gene expression data
+#'
+#' \code{mouse_diet} contains the mouse ID and diet information for the mice
+#'     used in this study.
+#'
+#' \code{moexpr_data} contains the gene expression of each mouse (one mouse
+#'     per column)
+#'
+#' This data was obtained from the Gene Expression Omnibus (GEO) under DataSet
+#'     Record GDS6248.
+#'
+#' "Analysis of livers of C57BL/6J mice fed a high fat diet for up to 24 weeks.
+#'     Significant body weight gain was observed after 4 weeks. Results provide
+#'     insight into the effect of high fat diets on metabolism in the liver."
+#'
+#' @format \code{moexpr_data} is a tibble (18,596 x 49)
+#' \describe{
+#'     \item{gene}{gene name for the row}
+#'     \item{GSM994790 ... GSM994837}{expression data for each mouse}
+#' }
+#'
+#' #' @format \code{mouse_diet} is a tibble (48 x 2)
+#' \describe{
+#'     \item{mouse}{the ID of the mouse}
+#'     \item{diet}{the level of fat in the diet}
+#' }
+#'
+#' @examples
+#' mouse_diet
+#' moexpr_data
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/sites/GDSbrowser/}
+"moexpr_data"
+
+#' @rdname moexpr_data
+"mouse_diet"
