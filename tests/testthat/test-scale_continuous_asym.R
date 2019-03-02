a13a737af85291d53b9d87ea759a64a8daccab26
Josh Cook <joshuacook0023@gmail.com>
2019-03-01 20:49:39 -0500

    test: finish tests for scale_fill_gradient fxns

--- tests/testthat/test-scale_continuous_asym.R
+++ tests/testthat/test-scale_continuous_asym.R
@@ -79,4 +79,49 @@ test_that("scale_fill_tl/br_gradient2 values populate properly", {
     expect_equal(g2_build$data[[2]]$fill_tl, rep(NA, nrow(tib)))
     expect_equal(g2_build$data[[2]]$fill_br, c("#AAC8D3", "#95A3A8", "#7F7F7F", "#7584A9", "#5F8AD3", "#1E90FF"))
 })
-# TODO: repeat for other fills
+
+
+test_that("scale_fill_tl/br_gradientn values populate properly", {
+    tib <- data.frame(grp1 = c("A", "A", "A", "B", "B", "C"),
+                      grp2 = c("B", "C", "D", "C", "D", "D"),
+                      val_1 = c(1, 2, NA, 0, 10, 5),
+                      val_2 = c(-2, -1, 0, 1, 2, 3))
+    g1 <- ggplot(tib) +
+        geom_asymmat(aes(x = grp1, y = grp2, fill_tl = val_1, fill_br = val_2)) +
+        scale_fill_tl_gradientn(colours = terrain.colors(10))
+    g2 <- g1 +
+        scale_fill_br_gradientn(colours = heat.colors(10))
+
+    g1_build <- ggplot2::ggplot_build(g1)
+    g2_build <- ggplot2::ggplot_build(g2)
+
+    expect_equal(g1$scales$n(), 1)
+    expect_true(g1$scales$has_scale("fill_tl"))
+    expect_false(g1$scales$has_scale("fill_br"))
+    expect_equal(g2$scales$n(), 2)
+    expect_true(g2$scales$has_scale("fill_tl"))
+    expect_true(g2$scales$has_scale("fill_br"))
+
+    expect_equal(g1_build$data[[1]]$fill_tl,c("#2AB400", "#5AC300", "grey50", "#00A600", "#F2F2F2", "#E7D520"))
+    expect_equal(g1_build$data[[1]]$fill_br, rep(NA, nrow(tib)))
+    expect_equal(g1_build$data[[2]]$fill_tl, rep(NA, nrow(tib)))
+    expect_equal(g1_build$data[[2]]$fill_br, tib$val_2)
+    expect_equal(g2_build$data[[1]]$fill_tl, c("#2AB400", "#5AC300", "grey50", "#00A600", "#F2F2F2", "#E7D520"))
+    expect_equal(g2_build$data[[1]]$fill_br, rep(NA, nrow(tib)))
+    expect_equal(g2_build$data[[2]]$fill_tl, rep(NA, nrow(tib)))
+    expect_equal(g2_build$data[[2]]$fill_br, c("#FF0000", "#FF4300", "#FF8400", "#FFC500", "#FFFF19", "#FFFFBF"))
+})
+
+
+test_that("ggplot's midrescaler is working", {
+    num_scale <- mid_rescaler(mid = 5)
+    v <- c(0:10)
+    sv <- seq(0, 1, 0.1)
+
+    expect_true(is.function(mid_rescaler(mid = 10)))
+    expect_true(is.function(mid_rescaler(mid = NA)))
+    expect_equal(num_scale(v), sv)
+    expect_error(num_scale("A"), regexp = "applied to an object of class")
+    expect_true(is.null(num_scale(c())))
+})
+
