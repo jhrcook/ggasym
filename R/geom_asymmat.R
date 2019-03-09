471f1f28c7bab212855b7c35aa98e20f957ba414
Josh Cook <joshuacook0023@gmail.com>
2019-03-09 11:18:02 -0500

    chore: make sure everything is production ready

--- R/geom_asymmat.R
+++ R/geom_asymmat.R
@@ -1,7 +1,8 @@
 #' Asymmetrically filled symmetric matrix (using ggplot)
 #'
 #' Generate an asymmetric matrix with different fill values for top-left
-#'     and bottom-right triangles as a \code{ggplot} object
+#'     and bottom-right triangles and along the diagonal as a
+#'     \code{ggplot} object
 #'
 #' @param mapping Set of aesthetic mappings created by aes() or aes_(). If
 #'     specified and \code{inherit.aes = TRUE} (the default), it is combined with
@@ -41,6 +42,7 @@
 #' @examples
 #' library(tibble)
 #' library(ggplot2)
+#' suppressWarnings(library(dplyr))
 #' tib <- tibble(g1 = c("A", "A", "B"),
 #'               g2 = c("B", "C", "C"),
 #'               val_1 = c(1, 2, 3),
@@ -49,10 +51,13 @@
 #' tib
 #'
 #' tib <- asymmetrise(tib, g1, g2)
-#' ggplot(tib) +
-#' geom_asymmat(aes(x = g1, y = g2, fill_tl = val_1, fill_br = val_2)) +
+#' tib$val_3 <- NA
+#' tib$val_3[tib$g1 == tib$g2] <- c(1, 2, 3)
+#' ggplot(tib, aes(x = g1, y = g2)) +
+#'     geom_asymmat(aes(fill_tl = val_1, fill_br = val_2, fill_diag = val_3)) +
 #'     scale_fill_br_gradient(low = "lightblue1", high = "dodgerblue") +
 #'     scale_fill_tl_gradient(low = "lightpink", high = "tomato") +
+#'     scale_fill_diag_gradient(low = "aquamarine", high = "forestgreen") +
 #'     labs(fill_tl =  "top-left fill", fill_br = "bottom-right fill")
 #'
 #' @importFrom stringr str_detect
@@ -64,13 +69,11 @@ geom_asymmat <- function(mapping = NULL, data = NULL,
                          na.rm = FALSE,
                          show.legend = NA,
                          inherit.aes = TRUE) {
-    # open mapping and pass fill_tl as fill to tl layer and fill_br to br layer
-    # browser()
+    # open mapping and remove tw of the three aesthetics
     mapping_1 <- mapping[!str_detect(names(mapping), "fill_br|fill_diag")]
     mapping_2 <- mapping[!str_detect(names(mapping), "fill_tl|fill_diag")]
     mapping_3 <- mapping[!str_detect(names(mapping), "fill_tl|fill_br")]
-    # names(mapping_2)[[1]] <- "y"
-    # names(mapping_2)[[2]] <- "x"
+
     new_layer1 <- layer(
         data = data,
         mapping = mapping_1,
@@ -147,11 +150,7 @@ GeomAsymmat <- ggproto(
     },
 
     draw_panel = function(self, data, panel_params, coord) {
-        # what I add to add the scaled fill_tl/br to `fill`
-        # TODO: I have removed the error by defaulting to `NA`, but still need
-        #   to figure out how the default blue is called
-        # TODO: this needs to be a better check that they are colors
-        #   make the check it's own function
+        # coordinate which fill_* gets used for fill
         if (all(is.character(data$fill_tl))) {
             data$fill <- data$fill_tl
         } else if (all(is.character(data$fill_br))) {
@@ -225,16 +224,8 @@ organize_xy <- function(data, params) {
     if (!any(names(params) == "which_triangle")) return(data)
     if (params$which_triangle == "tl") {
         data <- data %>% dplyr::filter(data$x < data$y)
-        # .new_x <- ifelse(data$x <= data$y, data$x, data$y)
-        # .new_y <- ifelse(data$x <= data$y, data$y, data$x)
-        # data$x <- .new_x
-        # data$y <- .new_y
     } else if (params$which_triangle == "br") {
         data <- data %>% dplyr::filter(data$x > data$y)
-        # .new_x <- ifelse(data$x >= data$y, data$x, data$y)
-        # .new_y <- ifelse(data$x >= data$y, data$y, data$x)
-        # data$x <- .new_x
-        # data$y <- .new_y
     } else if (params$which_triangle == "diag") {
         data <- data %>% dplyr::filter(data$x == data$y)
     }
